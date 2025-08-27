1c0dc71ce006283dc77a275901cdbd24ae9d6f4a
Daniel Quintão Féres <danielqferes@outlook.com>
2025-08-27 11:06:31 -0300

    to usando random agora

--- AEDII/aula_190825/vetores.py
+++ AEDII/aula_190825/vetores.py
@@ -1,14 +1,10 @@
-import gerador_de_numeros
 import arquivo
+import random
 
-gerador_de_numeros.iniciar_gerador(semente=21) # escolhi minha idade 
-vetor1 = gerador_de_numeros.gerar_varios_numeros(100)
-arquivo.salvar_txt(vetor1, './AEDII/190825/data/vetor1.txt')
+vetor1 = [random.randint(1, 1000) for _ in range(100)]
+vetor2 = vetor1 = [random.randint(1, 1000) for _ in range(1000)]
+vetor3 = vetor1 = [random.randint(1, 1000) for _ in range(10000)]
 
-gerador_de_numeros.iniciar_gerador(semente=1805) # diferente para dar numeros diferentes na 'sequencia pseudo-aleatoria' (escolhi dia e mes do meu aniversario)
-vetor2 = gerador_de_numeros.gerar_varios_numeros(1000)
-arquivo.salvar_txt(vetor2, './AEDII/190825/data/vetor2.txt')
-
-gerador_de_numeros.iniciar_gerador(semente=2004) # diferente para dar numeros diferentes na 'sequencia pseudo-aleatoria' (escolhi ano que nasci)
-vetor3 = gerador_de_numeros.gerar_varios_numeros(10000)
-arquivo.salvar_txt(vetor3, './AEDII/190825/data/vetor3.txt')
+arquivo.salvar_txt(vetor1, './AEDII/aula_190825/data/vetor1.txt')
+arquivo.salvar_txt(vetor2, './AEDII/aula_190825/data/vetor2.txt')
+arquivo.salvar_txt(vetor3, './AEDII/aula_190825/data/vetor3.txt')
