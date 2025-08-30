24e064ed5763519fcf9106d57a4db6325f65dcfc
Daniel Quintão Féres <danielqferes@outlook.com>
2025-08-29 21:35:18 -0300

    Comecei a estudar usando um dataset mais robusto

--- Pandas/Series e Dataframes/serie.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Dados
-cidades = ['São Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Salvador']
-populacoes = [12396372, 6775561, 2530701, 2900319]
-
-serie = pd.Series(data=populacoes, index=cidades)
-
-print(serie)
---
+++ Pandas/Titanic-Dataset/src/data_processing.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df_titanic = pd.read_csv("Pandas/Titanic-Dataset/data/raw/tested.csv")
+
+print(df_titanic.head)
